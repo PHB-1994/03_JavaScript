c4fc21b26ea23372be674e1c3ff4e33b0a7c1227
PHB-1994 <gudqls0991@naver.com>
2025-07-25 17:01:15 +0900

    행운의 로또 번호 뽑기 실습

---
+++ js/12_행운의번호뽑기.js
@@ -0,0 +1,34 @@
+/*
+const
+lottoResult
+lottoBtn
+
+id값을 이용해서
+click 기능을 눌렀을 때
+
+Math.floor(Math.random()*45) + 1;
+
+num1 ~ num6
+난수 생성해서
+innertext 로 화면 출력하기
+
+*/
+const lottoResult = document.getElementById("lottoResult");
+const lottoBtn = document.getElementById("lottobutton");
+
+lottoBtn.addEventListener("click", function () {
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    const num = Math.floor(Math.random() * 45) + 1;
+    if (i === 0) {
+      result = num;
+    } else if (i === 5) {
+      result = result + " + " + num;
+    } else {
+      result = result + ", " + num;
+    }
+    // 만약에 i가 5라면 " + "를 붙여서 num 추가하기
+  }
+
+  lottoResult.innerText = result;
+});
