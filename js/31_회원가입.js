1ea9fd1b7602f40022625acd60b14bc322128214
PHB-1994 <gudqls0991@naver.com>
2025-08-12 10:52:23 +0900

    cookie, session, local 개요, session 실습(로그인,로그아웃)

---
+++ js/31_회원가입.js
@@ -0,0 +1,30 @@
+// const $ = document.querySelector();
+$(function () {
+  $("#signup-btn").click(signFn);
+});
+
+function signFn() {
+  const username = $("#username").val();
+  const useremail = $("#useremail").val();
+
+  const user = {
+    // local 에 저장할 변수 이름 :
+    // js 로 사용자가 작성한 html input 안데 들어있는 value = 값을 갖고 있는 변수 이름
+    name: username,
+    email: useremail,
+  };
+
+  // 컴퓨터 웹 사이트에 사용자 정보를 저장할 때 문자열로 변환 해서 저장
+  localStorage.setItem("userInfo", JSON.stringify(user));
+
+  // alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+  alert(
+    "회원가입이 완료되었습니다. 로그인이 된 상태로 마이 페이지로 이동합니다."
+  );
+
+  // session 스토리지에 로그인 정보 저장
+  sessionStorage.setItem("loggedInUser", JSON.stringify(user));
+
+  // 로그인 완료되면 마이 페이지로 이동
+  window.location.href = "31_마이페이지.html";
+}
